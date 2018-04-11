bd2ab73a1b0cd426270ed052e92524d995132392
Benjamin Soddy <benjamin.soddy@gmail.com>
2018-04-10 19:11:45 -0500

    feature: xss protection; summary markdown

--- Data/Models/Content.cs
+++ Data/Models/Content.cs
@@ -1,26 +1,52 @@
 using System;
+using System.Web;
+using Ganss.XSS;
 
 namespace Diov.Data
 {
     public class Content
     {
-        private string _body;
+        private static IHtmlSanitizer htmlSanitizer =
+            new HtmlSanitizer();
+
+        private string body;
+        private string path;
+        private string summary;
 
         public string Body
         {
             get
             {
-                return _body;
+                return body;
             }
             set
             {
-                _body = value;  // TODO Sanitize on set
+                body = htmlSanitizer.Sanitize(value);
             }
         }
         public int Id { get; set; }
         public bool IsIndexed { get; set; }
         public string Name { get; set; }
-        public string Path { get; set; } // TODO Path encode on set
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                value = value.ToLowerInvariant();
+                var encodedPath = HttpUtility.UrlPathEncode(value);
+                if (!string.Equals(
+                    value,
+                    encodedPath,
+                    StringComparison.InvariantCulture))
+                {
+                    throw new ArgumentException();
+                }
+                path = encodedPath;
+            }
+        }
         public DateTimeOffset PublishedDateTime { get; set; }
         public string Summary { get; set; }
     }
