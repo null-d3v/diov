6b6a2bdd047cfdeac9975dcd57fae43de0f7bc4a
Benjamin Soddy <benjamin.soddy@gmail.com>
2018-04-16 00:24:24 -0500

    feature: better error pages

--- Web/Controllers/ErrorController.cs
+++ Web/Controllers/ErrorController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Diov.Data;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Diov.Web
 {
