df43ef585a75f5de7b093bddc657b52faa2e9b00
Benjamin Soddy <benjamin.soddy@gmail.com>
2020-05-04 14:44:39 -0500

    chore: format

--- Data/Models/AdminAuthorization.cs
+++ Data/Models/AdminAuthorization.cs
@@ -3,7 +3,6 @@ namespace Diov.Data
     public class AdminAuthorization
     {
         private string identityProvider;
-
         public string AccountId { get; set; }
         public int Id { get; set; }
         public string IdentityProvider
