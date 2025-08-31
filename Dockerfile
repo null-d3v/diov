9407689640108ab26842e5ce282de940cda3ccec
null-d3v <benjamin.soddy@gmail.com>
2025-08-31 15:02:18 -0500

    chore: updates

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
 WORKDIR /src
 
+COPY ./Directory.Build.props ./
+COPY ./Directory.Packages.props ./
 COPY src/Data/Diov.Data.csproj src/Data/
 COPY src/Web/Diov.Web.csproj src/Web/
 RUN dotnet restore -r linux-musl-x64 src/Web/Diov.Web.csproj
@@ -9,7 +11,7 @@ COPY . .
 WORKDIR /src/src/Web
 RUN dotnet publish -c Release --no-restore -o /app -r linux-musl-x64 --self-contained false
 
-FROM mcr.microsoft.com/dotnet/aspnet:8.0-alpine
+FROM mcr.microsoft.com/dotnet/aspnet:9.0-alpine
 
 ENV \
     DOTNET_HOSTBUILDER__RELOADCONFIGONCHANGE=false \
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -5,278 +5,231 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Caching.Distributed;
-using Serilog;
 using StackExchange.Redis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using WebMarkupMin.AspNetCore8;
+using WebMarkupMin.AspNetCoreLatest;
 
-#pragma warning disable CA1031
+var webApplicationBuilder = WebApplication
+    .CreateBuilder(args);
+webApplicationBuilder.Configuration
+    .AddJsonFile("appsettings.json", false, false)
+    .AddEnvironmentVariables();
 
-try
+if (!webApplicationBuilder.Environment.IsDevelopment())
 {
-    Log.Logger = new LoggerConfiguration()
-        .CreateBootstrapLogger();
+    webApplicationBuilder.Environment.WebRootFileProvider =
+        new NoWatchFileProvider(
+            webApplicationBuilder
+                .Environment
+                .WebRootFileProvider);
+}
 
-    var webApplicationBuilder = WebApplication
-        .CreateBuilder(args);
+webApplicationBuilder.Logging
+    .ClearProviders()
+    .AddJsonConsole();
+
+webApplicationBuilder.Services
+    .AddSingleton(webApplicationBuilder.Configuration);
+
+webApplicationBuilder.Services
+    .AddOptions();
+webApplicationBuilder.Services
+    .Configure<DistributedCacheEntryOptions>(
+        options =>
+        {
+            options.AbsoluteExpirationRelativeToNow =
+                TimeSpan.FromDays(1);
+        });
+webApplicationBuilder.Services
+    .Configure<ExternalAuthenticationOptions>(
     webApplicationBuilder.Configuration
-        .AddJsonFile("appsettings.json", false, false)
-        .AddEnvironmentVariables();
+        .GetSection("ExternalAuthentication"));
+webApplicationBuilder.Services
+    .Configure<JsonSerializerOptions>(
+        options =>
+        {
+            options.NumberHandling = JsonNumberHandling
+                .AllowReadingFromString;
+            options.PropertyNameCaseInsensitive = true;
+        });
 
-    if (!webApplicationBuilder.Environment.IsDevelopment())
-    {
-        webApplicationBuilder.Environment.WebRootFileProvider =
-            new NoWatchFileProvider(
-                webApplicationBuilder
-                    .Environment
-                    .WebRootFileProvider);
-    }
+webApplicationBuilder.Services
+    .AddSingleton<IDbConnectionFactory>(
+        new DbConnectionFactory(
+            webApplicationBuilder.Configuration
+                .GetConnectionString("Sql")!));
+webApplicationBuilder.Services
+    .AddTransient<
+        IAdminAuthorizationRepository,
+        AdminAuthorizationRepository>();
+webApplicationBuilder.Services
+    .AddTransient<IContentRepository, ContentRepository>();
+webApplicationBuilder.Services
+    .AddHostedService<MigrationHostedService>();
 
-    webApplicationBuilder.Host
-        .UseSerilog(
-            (context, loggerConfiguration) =>
-                loggerConfiguration
-                    .ReadFrom.Configuration(
-                        context.Configuration));
+webApplicationBuilder.Services
+    .AddL1L2RedisCache(
+        options =>
+        {
+            options.Configuration = webApplicationBuilder.Configuration
+                .GetConnectionString("Redis");
+            options.InstanceName = Constants.RedisInstanceName;
+        });
+webApplicationBuilder.Services
+    .AddTransient<
+        IContentAccessor,
+        DistributedCacheContentAccessor>();
 
-    webApplicationBuilder.Services
-        .AddSingleton(webApplicationBuilder.Configuration);
+webApplicationBuilder.Services
+    .Configure<ForwardedHeadersOptions>(
+        options =>
+        {
+            options.ForwardedHeaders =
+                ForwardedHeaders.XForwardedFor |
+                ForwardedHeaders.XForwardedProto;
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
+webApplicationBuilder.Services
+    .AddResponseCompression();
+webApplicationBuilder.Services
+    .AddWebOptimizer(
+        options =>
+        {
+            options.EnableDiskCache = false;
+        },
+        !webApplicationBuilder.Environment.IsDevelopment(),
+        !webApplicationBuilder.Environment.IsDevelopment());
+webApplicationBuilder.Services
+    .AddWebMarkupMin()
+    .AddHtmlMinification()
+    .AddHttpCompression();
 
-    webApplicationBuilder.Services
-        .AddOptions();
-    webApplicationBuilder.Services
-        .Configure<DistributedCacheEntryOptions>(
-            options =>
-            {
-                options.AbsoluteExpirationRelativeToNow =
-                    TimeSpan.FromDays(1);
-            });
-    webApplicationBuilder.Services
-        .Configure<ExternalAuthenticationOptions>(
-        webApplicationBuilder.Configuration
-            .GetSection("ExternalAuthentication"));
-    webApplicationBuilder.Services
-        .Configure<JsonSerializerOptions>(
-            options =>
-            {
-                options.NumberHandling = JsonNumberHandling
-                    .AllowReadingFromString;
-                options.PropertyNameCaseInsensitive = true;
-            });
-
-    webApplicationBuilder.Services
-        .AddSingleton<IDbConnectionFactory>(
-            new DbConnectionFactory(
+webApplicationBuilder.Services
+    .AddDataProtection()
+    .PersistKeysToStackExchangeRedis(
+        () => ConnectionMultiplexer
+            .Connect(
                 webApplicationBuilder.Configuration
-                    .GetConnectionString("Sql")!));
-    webApplicationBuilder.Services
-        .AddTransient<
-            IAdminAuthorizationRepository,
-            AdminAuthorizationRepository>();
-    webApplicationBuilder.Services
-        .AddTransient<IContentRepository, ContentRepository>();
-    webApplicationBuilder.Services
-        .AddHostedService<MigrationHostedService>();
+                    .GetConnectionString("Redis")!)
+            .GetDatabase(),
+            $"{Constants.RedisInstanceName}DataProtection-Keys");
 
-    webApplicationBuilder.Services
-        .AddL1L2RedisCache(
-            options =>
-            {
-                options.Configuration = webApplicationBuilder.Configuration
-                    .GetConnectionString("Redis");
-                options.InstanceName = Constants.RedisInstanceName;
-            });
-    webApplicationBuilder.Services
-        .AddTransient<
-            IContentAccessor,
-            DistributedCacheContentAccessor>();
-
-    webApplicationBuilder.Services
-        .Configure<ForwardedHeadersOptions>(
-            options =>
-            {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor |
-                    ForwardedHeaders.XForwardedProto;
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
-            });
-    webApplicationBuilder.Services
-        .AddResponseCompression();
-    webApplicationBuilder.Services
-        .AddWebOptimizer(
-            assetPipeline =>
-            {
-                assetPipeline.MinifyCssFiles(
-                    "css/**/*.css");
-                assetPipeline.MinifyJsFiles(
-                    "js/**/*.js");
-                assetPipeline.AddCssBundle(
-                    "/css/site.min.css",
-                    "css/**/*.css");
-                assetPipeline.AddJavaScriptBundle(
-                    "/js/site.min.js",
-                    "js/**/*.js");
-            },
-            options =>
-            {
-                options.EnableDiskCache = false;
-            });
-    webApplicationBuilder.Services
-        .AddWebMarkupMin(
-            options =>
-            {
-                options.AllowMinificationInDevelopmentEnvironment = true;
-                options.AllowCompressionInDevelopmentEnvironment = true;
-            })
-        .AddHtmlMinification()
-        .AddHttpCompression();
-
-    webApplicationBuilder.Services
-        .AddDataProtection()
-        .PersistKeysToStackExchangeRedis(
-            () => ConnectionMultiplexer
-                .Connect(
-                    webApplicationBuilder.Configuration
-                        .GetConnectionString("Redis")!)
-                .GetDatabase(),
-                $"{Constants.RedisInstanceName}DataProtection-Keys");
-
-    var externalAuthenticationOptions = webApplicationBuilder.Configuration
-        .GetSection("ExternalAuthentication")
-        .Get<ExternalAuthenticationOptions>();
-    var authenticationBuilder = webApplicationBuilder.Services
-        .AddAuthentication(
-            Constants.LocalAuthenticationScheme)
-        .AddCookie(
-            Constants.LocalAuthenticationScheme,
-            cookieOptions =>
-            {
-                cookieOptions.LoginPath = "/auth/login";
-            })
-        .AddCookie(
-            Constants.ExternalAuthenticationScheme);
-    if (!string.IsNullOrEmpty(
-            externalAuthenticationOptions?.Google?.ClientId) &&
-        !string.IsNullOrEmpty(
-            externalAuthenticationOptions?.Google?.ClientSecret))
-    {
-        authenticationBuilder
-            .AddGoogle(
-                googleOptions =>
-                {
-                    googleOptions.ClientId = externalAuthenticationOptions
-                        .Google.ClientId;
-                    googleOptions.ClientSecret = externalAuthenticationOptions
-                        .Google.ClientSecret;
-                    googleOptions.SignInScheme = Constants
-                        .ExternalAuthenticationScheme;
-                });
-    }
-    webApplicationBuilder.Services
-        .AddTransient<
-            IAuthenticationSchemeProvider,
-            IgnoreCaseAuthenticationSchemeProvider>();
-
-    webApplicationBuilder.Services
-        .AddRouting(
-            routeOptions =>
-            {
-                routeOptions.LowercaseUrls = true;
-            });
-
-    webApplicationBuilder.Services
-        .AddHealthChecks()
-        .AddRedis(
-            webApplicationBuilder.Configuration
-                .GetConnectionString("Redis")!,
-            tags: [ Constants.ReadinessHealthCheckTag, ])
-        .AddSqlServer(
-            webApplicationBuilder.Configuration
-                .GetConnectionString("Sql")!,
-            tags: [ Constants.ReadinessHealthCheckTag, ]);
-
-    var mvcBuilder = webApplicationBuilder.Services
-        .AddControllersWithViews();
-    if (webApplicationBuilder.Environment.IsDevelopment())
-    {
-        mvcBuilder.AddRazorRuntimeCompilation();
-    }
-
-    using var webApplication = webApplicationBuilder
-        .Build();
-
-    webApplication
-        .UseSerilogRequestLogging();
-
-    if (webApplicationBuilder.Environment.IsDevelopment())
-    {
-        webApplication
-            .UseDeveloperExceptionPage();
-    }
-    else
-    {
-        webApplication
-            .UseExceptionHandler("/error/500");
-        webApplication
-            .UseStatusCodePagesWithReExecute("/error/{0}");
-    }
-
-    webApplication
-        .UseResponseCompression()
-        .UseWebOptimizer()
-        .UseWebMarkupMin()
-        .UseStaticFiles(
-            new StaticFileOptions
-            {
-                ServeUnknownFileTypes = true,
-            })
-        .UseRouting();
-
-    webApplication
-        .UseForwardedHeaders();
-
-    webApplication
-        .UseAuthentication()
-        .UseAuthorization();
-
-    webApplication
-        .MapHealthChecks(
-            "/health/live",
-            new HealthCheckOptions
-            {
-                Predicate = (healthCheckRegistration) => false,
-            });
-    webApplication
-        .MapHealthChecks(
-        "/health/ready",
-            new HealthCheckOptions
-            {
-                Predicate = (healthCheckRegistration) =>
-                    healthCheckRegistration.Tags.Contains(
-                        Constants.ReadinessHealthCheckTag),
-            });
-    webApplication
-        .MapControllers();
-
-    await webApplication
-        .RunAsync()
-        .ConfigureAwait(false);
-    return 0;
-}
-catch (Exception exception)
+var externalAuthenticationOptions = webApplicationBuilder.Configuration
+    .GetSection("ExternalAuthentication")
+    .Get<ExternalAuthenticationOptions>();
+var authenticationBuilder = webApplicationBuilder.Services
+    .AddAuthentication(
+        Constants.LocalAuthenticationScheme)
+    .AddCookie(
+        Constants.LocalAuthenticationScheme,
+        cookieOptions =>
+        {
+            cookieOptions.LoginPath = "/auth/login";
+        })
+    .AddCookie(
+        Constants.ExternalAuthenticationScheme);
+if (!string.IsNullOrEmpty(
+        externalAuthenticationOptions?.Google?.ClientId) &&
+    !string.IsNullOrEmpty(
+        externalAuthenticationOptions?.Google?.ClientSecret))
 {
-    Log.Fatal(
-        exception,
-        "Host terminated unexpectedly");
-    return 1;
+    authenticationBuilder
+        .AddGoogle(
+            googleOptions =>
+            {
+                googleOptions.ClientId = externalAuthenticationOptions
+                    .Google.ClientId;
+                googleOptions.ClientSecret = externalAuthenticationOptions
+                    .Google.ClientSecret;
+                googleOptions.SignInScheme = Constants
+                    .ExternalAuthenticationScheme;
+            });
 }
-finally
+webApplicationBuilder.Services
+    .AddTransient<
+        IAuthenticationSchemeProvider,
+        IgnoreCaseAuthenticationSchemeProvider>();
+
+webApplicationBuilder.Services
+    .AddRouting(
+        routeOptions =>
+        {
+            routeOptions.LowercaseUrls = true;
+        });
+
+webApplicationBuilder.Services
+    .AddHealthChecks()
+    .AddRedis(
+        webApplicationBuilder.Configuration
+            .GetConnectionString("Redis")!,
+        tags: [ Constants.ReadinessHealthCheckTag, ])
+    .AddSqlServer(
+        webApplicationBuilder.Configuration
+            .GetConnectionString("Sql")!,
+        tags: [ Constants.ReadinessHealthCheckTag, ]);
+
+var mvcBuilder = webApplicationBuilder.Services
+    .AddControllersWithViews();
+if (webApplicationBuilder.Environment.IsDevelopment())
 {
-    await Log
-        .CloseAndFlushAsync()
-        .ConfigureAwait(false);
+    mvcBuilder.AddRazorRuntimeCompilation();
 }
 
-#pragma warning restore CA1031
+using var webApplication = webApplicationBuilder
+    .Build();
+
+if (webApplicationBuilder.Environment.IsDevelopment())
+{
+    webApplication
+        .UseDeveloperExceptionPage();
+}
+else
+{
+    webApplication
+        .UseExceptionHandler("/error/500");
+    webApplication
+        .UseStatusCodePagesWithReExecute("/error/{0}");
+}
+
+webApplication
+    .UseResponseCompression()
+    .UseWebOptimizer()
+    .UseWebMarkupMin()
+    .UseStaticFiles(
+        new StaticFileOptions
+        {
+            ServeUnknownFileTypes = true,
+        })
+    .UseRouting();
+
+webApplication
+    .UseForwardedHeaders();
+
+webApplication
+    .UseAuthentication()
+    .UseAuthorization();
+
+webApplication
+    .MapHealthChecks(
+        "/health/live",
+        new HealthCheckOptions
+        {
+            Predicate = (healthCheckRegistration) => false,
+        });
+webApplication
+    .MapHealthChecks(
+    "/health/ready",
+        new HealthCheckOptions
+        {
+            Predicate = (healthCheckRegistration) =>
+                healthCheckRegistration.Tags.Contains(
+                    Constants.ReadinessHealthCheckTag),
+        });
+webApplication
+    .MapControllers();
+
+await webApplication
+    .RunAsync()
+    .ConfigureAwait(false);
